0bafc98df5ef64ee945406afdc973a8b42172dfa
terraformmedia <31185372+terraformmedia@users.noreply.github.com>
2020-05-10 17:06:27 +0100

    switch all other headings to h2 tags for accessibility

--- js/script.js
+++ js/script.js
@@ -4,7 +4,9 @@ const darkToggle = document.getElementById('toggleButton');
 const toggleBall = document.getElementById('toggleBall');
 
 toggleButton.addEventListener('click', () => {
+    //Toggle between light and dark stylesheets on click of toggle button
     stylesheet.href.match('style.css') ? stylesheet.setAttribute('href', 'css/styleDark.css') : stylesheet.setAttribute('href', 'css/style.css');
+    //Animate the toggle button ball to the right, to give that switch effect
     toggleBall.classList.toggle('ballToRight');
     toggleBall.style.transition = " 0.3s ease-in-out";
 })
